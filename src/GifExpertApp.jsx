fb7d1fcaf6a9f237d2cda237d5d8c1dba9324d02
Alvaro Solano <alvarocr21@gmail.com>
2023-11-04 18:32:04 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    // const valor = "HunterXHunter";
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory
+        //setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
+
+      {/*Listado de Gif  */}
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGif = async (category) => {
+  const apiKey = "vKcdVS8MNNbTY3xnp03nyprRznMRN0AJ";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gif = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  return gif;
+};
